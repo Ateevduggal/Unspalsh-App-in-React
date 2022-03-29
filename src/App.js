24439157bf91ae65115e418455fa1207a8392c8b
AteevDuggal <akd3257@gmail.com>
2022-03-29 12:03:15 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [img, setImg] = useState("");
+  const [res, setRes] = useState([]);
+
+  const fetchRequest = async () => {
+    const data = await fetch(
+      `https://api.unsplash.com/search/photos?page=1&query=${img}&client_id=e87p8Mo2TddFOvRqri0c0bFXJWQM9RxqLOWZpuMNN-4&per_page=20`
+    );
+    const dataJ = await data.json();
+    const result = dataJ.results;
+    console.log(result);
+    setRes(result);
+  };
+
+  useEffect(() => {
+    fetchRequest();
+  }, []);
+
+  const Submit = () => {
+    fetchRequest();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12 d-flex justify-content-center align-items-center input">
+            <input
+              className="col-3 form-control-sm py-1 fs-4 text-capitalize border border-3 border-dark"
+              type="text"
+              placeholder="Search Anything..."
+              value={img}
+              onChange={(e) => setImg(e.target.value)}
+            />
+            <button
+              type="submit"
+              onClick={Submit}
+              className="btn bg-dark text-white fs-3 mx-3"
+            >
+              Search
+            </button>
+          </div>
+          <div className="col-12 d-flex justify-content-evenly flex-wrap">
+            {res.map((val) => {
+              return (
+                <>
+                  <img
+                    key={val.id}
+                    className="col-3 img-fluid img-thumbnail"
+                    src={val.urls.small}
+                    alt=""
+                  />
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
